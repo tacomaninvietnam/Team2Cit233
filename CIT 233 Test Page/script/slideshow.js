a3b751b4153588a5b3d5f75cf1ec33823ec0758b
tacomaninvietnam <twostrippersandacrackpipe@yahoo.com>
2014-11-29 20:29:16 -0500

    added team2, TC outsiders site
    Frankfort and Glen Arbor are done, test them but we need cover page,
    contact form, and legal html file s added

---
+++ CIT 233 Test Page/script/slideshow.js
@@ -0,0 +1,22 @@
+﻿
+$(function(){
+    $('#fadein img:gt(0), #fadein2 img:gt(0), #fadein3 img:gt(0), #fadein4 img:gt(0), #mainWindow img:gt(0)').hide();
+	setInterval(function(){
+	    $('#fadein :first-child').fadeOut(000)
+		    .next('img').fadeIn(1000)
+		    .end().appendTo('#fadein');
+	    $('#fadein2 :first-child').fadeOut(000)
+            .next('img').fadeIn(1000)
+            .end().appendTo('#fadein2');
+	    $('#fadein3 :first-child').fadeOut(000)
+            .next('img').fadeIn(1000)
+            .end().appendTo('#fadein3');
+	    $('#fadein4 :first-child').fadeOut(000)
+            .next('img').fadeIn(1000)
+            .end().appendTo('#fadein4');
+	    $('#mainWindow :first-child').fadeOut(000)
+            .next('img').fadeIn(1000)
+            .end().appendTo('#mainWindow');
+	}, 2000);
+});
+
